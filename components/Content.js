9db69062752767fc4078bf7d6e7da3a939e1b2ce
Themba <mndebele.themba@gmail.com>
2022-11-17 13:48:17 +0200

    Content component UI Fixes

--- components/Content.js
+++ components/Content.js
@@ -4,18 +4,18 @@ import Image from "next/image";
 
 const Item = ({ pictures, range, min_price, min_price_monthly }) => {
   return (
-    <div className=" border-gray-700 rounded-xl overflow-hidden">
-      <div className=" bg-gray-200">
+    <div className=" border-gray-200 border rounded-xl overflow-hidden">
+      <div className=" bg-gray-200 relative w-[340px] h-[180px]">
         <Image
           src={pictures[0]?.pictureWebPURL ?? pictures[0]?.pictureNormalURL}
           alt=""
-          width={360}
-          height={240}
+          fill
+          style={{ objectFit: "cover" }}
         />
       </div>
-      <div className="py-4 px-2">
+      <div className="py-4 px-3">
         <h3 className="font-bold text-xl mb-1">{range}</h3>
-        <p className="text-xs">
+        <p className="text-xs mb-3">
           <span>From</span>{" "}
           <span className="font-bold">{`R ${min_price.toLocaleString(
             "en-ZA"
@@ -25,6 +25,9 @@ const Item = ({ pictures, range, min_price, min_price_monthly }) => {
             .toFixed(0)
             .toLocaleString("en-ZA")}`}</span>
         </p>
+        <a href="#" className="text-sm underline-offset-2 underline">
+          Learn more
+        </a>
       </div>
     </div>
   );
@@ -53,7 +56,7 @@ const Content = () => {
         <h1 className="text-3xl font-bold text-center">Vehicles</h1>
         <p className="text-center">23 vehicles in stock</p>
       </div>
-      <div className="flex justify-center flex-wrap max-w-[900px] gap-6">
+      <div className="flex justify-center flex-wrap xl:max-w-[1200px] gap-6">
         {items.map((item, i) => (
           <Item key={i} {...item} />
         ))}
